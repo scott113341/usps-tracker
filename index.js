536a29d35e5873f318466aa372208d3756469284
Scott Hardy <scott.the.hardy@gmail.com>
2014-10-17 04:19:59 -0700

    wrote code to extract delivery status from the usps website

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var request = require('request');
+var cheerio = require('cheerio');
 var csv = require('csv');
 var fs = require('fs');
 
@@ -15,7 +17,7 @@ var inputString = "" +
   "Some random address line blah\n" +
   "Some random address line blah\n" +
   "Some random address line blah\n" +
-  "9410 8036 9930 0060 6497 78 (Sequence Number 1 of 1)\n" +
+  "9410 8036 9930 0060 6496 78 (Sequence Number 1 of 1)\n" +
   "Some random address line blah\n" +
   "Some random address line blah\n" +
   "Some random address line blah\n" +
@@ -32,17 +34,55 @@ var outputArray = [[
   parse input and get tracking numbers
  */
 
+var trackingNumbers = [];
 var lines = inputString.split("\n");
 lines.forEach(function(line, i) {
   var number_regex = /\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{2}/;
   var result = number_regex.exec(line);
 
   if (result) {
-    var outputLine = [result[0], 'Not yet known'];
-    outputArray.push(outputLine);
+    trackingNumbers.push(result[0]);
   }
 });
-console.log('outputArray', outputArray);
+console.log(trackingNumbers);
+
+
+
+/*
+  get tracking information from usps
+ */
+var completedRequests = 0;
+
+trackingNumbers.forEach(function(trackingNumber) {
+  var url = 'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=' + trackingNumber.split(' ').join('');
+  console.log(url);
+
+  request(url, function(error, response, body) {
+    completedRequests = completedRequests + 1;
+    console.log(completedRequests + ' completed requests');
+
+    var outputLine;
+    var deliveryStatus;
+
+    if (error) {
+      deliveryStatus = 'Request error!';
+    }
+    else {
+      deliveryStatus = 'parsing html';
+
+      var $ = cheerio.load(body);
+      deliveryStatus = $('p.info-text.first').first().text().trim();
+    }
+
+    outputLine = [trackingNumber, deliveryStatus];
+    outputArray.push(outputLine);
+
+    if (completedRequests === trackingNumbers.length) {
+      console.log('all requests done, writing csv');
+      writeCSV();
+    }
+  });
+});
 
 
 
@@ -50,15 +90,19 @@ console.log('outputArray', outputArray);
   write results to output.csv file
  */
 
-csv.stringify(outputArray, function(err, data) {
-  fs.writeFile("output.csv", data, function(err) {
-    if(err) {
-      console.log(err);
-    }
-    else {
-      console.log('output.csv saved!');
-    }
+var writeCSV = function() {
+  console.log('outputArray', outputArray);
 
-    console.log('program finished');
+  csv.stringify(outputArray, function(err, data) {
+    fs.writeFile("output.csv", data, function(err) {
+      if(err) {
+        console.log(err);
+      }
+      else {
+        console.log('output.csv saved!');
+      }
+
+      console.log('program finished');
+    });
   });
-});
+};
