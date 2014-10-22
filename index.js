c64a45dd0acc9233ec9f2d78b4868d4d5f96db6b
Scott Hardy <scott.the.hardy@gmail.com>
2014-10-21 22:36:04 -0700

    added input parsing of input.txt file

--- index.js
+++ index.js
@@ -11,17 +11,7 @@ var fs = require('fs');
   set up input and output
  */
 
-var inputString = "" +
-  "9410 8036 9930 0060 6497 77 (Sequence Number 1 of 1)\n" +
-  "Some random address line blah\n" +
-  "Some random address line blah\n" +
-  "Some random address line blah\n" +
-  "Some random address line blah\n" +
-  "9410 8036 9930 0060 6496 78 (Sequence Number 1 of 1)\n" +
-  "Some random address line blah\n" +
-  "Some random address line blah\n" +
-  "Some random address line blah\n" +
-  "Some random address line blah\n";
+var inputString = '';
 
 var outputArray = [[
   'Tracking Number',
@@ -31,58 +21,77 @@ var outputArray = [[
 
 
 /*
+  read input.txt
+ */
+fs.readFile('input.txt', 'utf8', function(err, data) {
+  if (err) {
+    console.log(err);
+  }
+
+  inputString = data;
+  parseInput();
+});
+
+
+
+/*
   parse input and get tracking numbers
  */
 
 var trackingNumbers = [];
-var lines = inputString.split("\n");
-lines.forEach(function(line, i) {
-  var number_regex = /\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{2}/;
-  var result = number_regex.exec(line);
 
-  if (result) {
-    trackingNumbers.push(result[0]);
-  }
-});
-console.log(trackingNumbers);
+var parseInput = function() {
+  var lines = inputString.split("\n");
+  lines.forEach(function(line, i) {
+    var number_regex = /\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{2}/;
+    var result = number_regex.exec(line);
+
+    if (result) {
+      trackingNumbers.push(result[0]);
+    }
+  });
+
+  getTrackingInfo();
+};
 
 
 
 /*
   get tracking information from usps
  */
-var completedRequests = 0;
+var getTrackingInfo = function() {
+  var completedRequests = 0;
 
-trackingNumbers.forEach(function(trackingNumber) {
-  var url = 'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=' + trackingNumber.split(' ').join('');
-  console.log(url);
+  trackingNumbers.forEach(function(trackingNumber) {
+    var url = 'https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=' + trackingNumber.split(' ').join('');
 
-  request(url, function(error, response, body) {
-    completedRequests = completedRequests + 1;
-    console.log(completedRequests + ' completed requests');
+    request(url, function(err, response, body) {
+      completedRequests = completedRequests + 1;
+      console.log(completedRequests + ' completed requests');
 
-    var outputLine;
-    var deliveryStatus;
+      var outputLine;
+      var deliveryStatus;
 
-    if (error) {
-      deliveryStatus = 'Request error!';
-    }
-    else {
-      deliveryStatus = 'parsing html';
+      if (err) {
+        deliveryStatus = 'Request error!';
+      }
+      else {
+        deliveryStatus = 'parsing html';
 
-      var $ = cheerio.load(body);
-      deliveryStatus = $('p.info-text.first').first().text().trim();
-    }
+        var $ = cheerio.load(body);
+        deliveryStatus = $('p.info-text.first').first().text().trim();
+      }
 
-    outputLine = [trackingNumber, deliveryStatus];
-    outputArray.push(outputLine);
+      outputLine = [trackingNumber, deliveryStatus];
+      outputArray.push(outputLine);
 
-    if (completedRequests === trackingNumbers.length) {
-      console.log('all requests done, writing csv');
-      writeCSV();
-    }
+      if (completedRequests === trackingNumbers.length) {
+        console.log('all requests done, writing csv');
+        writeCSV();
+      }
+    });
   });
-});
+};
 
 
 
@@ -94,15 +103,13 @@ var writeCSV = function() {
   console.log('outputArray', outputArray);
 
   csv.stringify(outputArray, function(err, data) {
-    fs.writeFile("output.csv", data, function(err) {
+    fs.writeFile('output.csv', data, function(err) {
       if(err) {
         console.log(err);
       }
       else {
         console.log('output.csv saved!');
       }
-
-      console.log('program finished');
     });
   });
 };
